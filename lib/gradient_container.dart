0ce33090993013b9c6435fbeef1ea49dba6b54f9
Angelo Filho <angelo.filho06@hotmail.com>
2023-06-29 17:32:21 -0300

    feat: add assets and load images

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -1,16 +1,13 @@
-import 'package:first_app/styled_text.dart';
 import 'package:flutter/material.dart';
 
 class GradientContainer extends StatelessWidget {
   const GradientContainer(this.colors, {super.key});
 
-  const GradientContainer.purple({super.key})
-      : colors = const [
-          Color.fromARGB(255, 22, 1, 71),
-          Color.fromARGB(255, 62, 11, 133),
-        ];
-
   final List<Color> colors;
+
+  void rollDice() {
+    print("oin");
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -22,9 +19,29 @@ class GradientContainer extends StatelessWidget {
           end: Alignment.bottomRight,
         ),
       ),
-      child: const Center(
-        child: StyledText("Novo trecho"),
-      ),
+      child: Center(
+          child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Image.asset(
+            "assets/images/dice-1.png",
+            width: 200,
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+          TextButton(
+              onPressed: rollDice,
+              style: TextButton.styleFrom(
+                // padding: const EdgeInsets.only(top: 20),
+                foregroundColor: Colors.white,
+                textStyle: const TextStyle(
+                  fontSize: 28,
+                ),
+              ),
+              child: const Text("Roll Dice"))
+        ],
+      )),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,11 @@ import 'package:first_app/gradient_container.dart';
 void main() {
   runApp(
     const MaterialApp(
-      home: Scaffold(body: GradientContainer.purple()),
+      home: Scaffold(
+          body: GradientContainer([
+        Color.fromARGB(255, 22, 1, 71),
+        Color.fromARGB(255, 62, 11, 133),
+      ])),
     ),
   );
 }
